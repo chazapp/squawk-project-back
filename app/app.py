97d6842d94dbefb5ef29eaca4efe3bf073c1edf6
Shad <shad@chaz.pro>
2019-04-02 15:34:53 +0200

    edited app location to fix production 404

--- app/app.py
+++ app/app.py
@@ -1,19 +1,14 @@
-from flask_pymongo import PyMongo
-import os
 from flask import Flask, g
 from flask_jwt_extended import JWTManager
-from flask_pymongo import PyMongo
-from dotenv import load_dotenv
 from app.auth import bp as auth
 
 from app.sources import bp as sources
 import os
 from dotenv import load_dotenv
 
-app = Flask(__name__, instance_relative_config=True)
-
 
 def create_app(testing=False):
+    app = Flask(__name__, instance_relative_config=True)
     if not testing:
         load_dotenv()
         app.config.from_mapping(
@@ -37,8 +32,9 @@ def create_app(testing=False):
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.app_context().push()
     app.run(host='0.0.0.0')
 
